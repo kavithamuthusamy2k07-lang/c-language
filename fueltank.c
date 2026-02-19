7f880a733ca2e24865c9b053292da4b3f3f05247
kavitha <kavithamuthusamy2k07@gmail.com>
2026-02-19 10:36:09 +0530

    Add files via upload

---
+++ fueltank.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int tank_filled;
+    scanf("%d",&tank_filled);
+
+    int total_hrs;
+    scanf("%d",&total_hrs);
+    int i=1;
+    int per_hrs;
+    while( i<=total_hrs){
+        
+
+    }
+}
