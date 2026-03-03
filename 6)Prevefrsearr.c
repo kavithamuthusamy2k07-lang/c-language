fb4f173c10ab705558fc4400a2608d4c4aa94ca5
kavitha <kavithamuthusamy2k07@gmail.com>
2026-03-03 16:09:15 +0530

    Add files via upload

---
+++ 6)Prevefrsearr.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+    scanf("%d",arr+i);
+    }
+    int i=0;
+    int *start=arr;
+    int *end=arr+n-1;
+    while(start<end){
+        int temp=*start;
+        *start=*end;
+        *end=temp;
+        start++;
+        end--;
+    }
+    for(int i=0;i<n;i++){
+    printf("%d ",*(arr+i));
+    }
+    return 0;
+}
+    
