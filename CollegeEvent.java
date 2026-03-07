d2c5736f0f8cdff238b51ad79b843df1d2685171
kavitha <kavithamuthusamy2k07@gmail.com>
2026-03-07 17:45:34 +0530

    Add files via upload

---
+++ CollegeEvent.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class CollegeEvent {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int registration=sc.nextInt();
+         int sponsorship =sc.nextInt();
+         int stall =sc.nextInt();
+         int stage =sc.nextInt();
+         int celebrity =sc.nextInt();
+         int marketing=sc.nextInt();
+         int Rfund=registration+sponsorship+stall-stage-celebrity-marketing;
+         System.out.print(Rfund);
+         sc.close();
+         
+    }
+}
